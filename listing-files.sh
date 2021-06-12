3d506084418408b1ae009590f05b172ef49cff10
jajati007 <token8369@gmail.com>
2021-06-12 20:18:50 +0530

    this is the first reverse for the last commit Revert "commit for ci_build testing"
    This reverts commit bdfa6a826c1ed1a767bf14e41108820b7c8b6919.

--- listing-files.sh
+++ listing-files.sh
@@ -6,4 +6,3 @@ echo "listing all the files with current date, time"
 ls -ltr >> /home/ec2-user/Testing/logs/daily/logs.`date +\%Y\%m\%d`
 
 #Set this script in Cron for specific time.
-#This is the file listing script in linux
