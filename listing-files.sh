b322b9e6fe530e0aa564e3dbf6f715d6ace52f79
Jajati Behera <token8369@gmail.com>
2023-06-30 01:39:21 +0530

    this is for testing

--- listing-files.sh
+++ listing-files.sh
@@ -5,4 +5,6 @@ echo "listing all the files with current date, time"
 
 ls -ltr >> /home/ec2-user/Testing/logs/daily/logs.`date +\%Y\%m\%d`
 
+echo "script execution has been done"
+
 #Set this script in Cron for specific time.
